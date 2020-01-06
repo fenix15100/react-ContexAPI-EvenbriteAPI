322768a4e203db80c7661e492ec29f4c29ce7cd5
fenix15100 <francisco.camacho15@gmail.com>
2020-01-06 09:46:46 +0100

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
+    <Header/>
     
-    <h2>It,Works</h2>
   );
 }
 
